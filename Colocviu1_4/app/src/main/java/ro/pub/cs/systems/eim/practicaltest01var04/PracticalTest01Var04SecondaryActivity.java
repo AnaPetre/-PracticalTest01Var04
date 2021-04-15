3d99bfe4b35d5c4fb2351c02e153971136d803ab
AnaPetre <ana_gabriela.petre@stud.acs.upb.ro>
2021-04-15 10:06:23 +0300

    Started C1

--- Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04SecondaryActivity.java
+++ Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04SecondaryActivity.java
@@ -2,13 +2,43 @@ package ro.pub.cs.systems.eim.practicaltest01var04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PracticalTest01Var04SecondaryActivity extends AppCompatActivity {
 
+    private Button okButton, cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var04_secondary);
+
+        okButton = (Button)findViewById(R.id.okButton);
+        cancelButton = (Button)findViewById(R.id.cancelButton);
+
+
+        okButton.setOnClickListener(buttonClickListener);
+        cancelButton.setOnClickListener(buttonClickListener);
+
+        Intent intent = getIntent();
+    }
+
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.okButton:
+                    setResult(RESULT_OK, null);
+                    break;
+                case R.id.cancelButton:
+                    setResult(RESULT_CANCELED, null);
+                    break;
+            }
+            finish();
+        }
     }
 }
