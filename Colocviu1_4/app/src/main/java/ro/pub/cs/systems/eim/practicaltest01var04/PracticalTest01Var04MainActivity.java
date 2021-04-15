4c081f061cec2adb106fc5b92f35ff4386fbcee1
AnaPetre <ana_gabriela.petre@stud.acs.upb.ro>
2021-04-15 10:10:23 +0300

    Started C1

--- Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -2,6 +2,7 @@ package ro.pub.cs.systems.eim.practicaltest01var04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -82,6 +83,10 @@ public class PracticalTest01Var04MainActivity extends AppCompatActivity {
 
             if(view.getId() == R.id.display_information && group_check.isChecked()) {
                 displayText = displayText + grupa;
+            }
+
+            if (view.getId() == R.id.navigate_to_second_activity) {
+                Intent intent = new Intent(getApplicationContext(), PracticalTest01Var04SecondaryActivity.class);
             }
 
             display_information_text.setText(displayText);
