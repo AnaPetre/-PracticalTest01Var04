b4f84c7a0e90177c0546ffdefe85ef835d4607c6
AnaPetre <ana_gabriela.petre@stud.acs.upb.ro>
2021-04-15 09:35:48 +0300

    Colocviu1_4

---
+++ Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/Constants.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest01var04;
+
+public interface Constants {
+    final public static String USERNAME_EDIT_TEXT   = "name";
+    final public static String GROUP_EDIT_TEXT   = "grupa";
+    final public static String REMEMBER_NAME = "name_check";
+    final public static String REMEMBER_GROUP = "group_check";
+}
--- Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ Colocviu1_4/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -3,12 +3,115 @@ package ro.pub.cs.systems.eim.practicaltest01var04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PracticalTest01Var04MainActivity extends AppCompatActivity {
+
+    private Button navigate_to_second_activity;
+    private Button display_information;
+    private TextView display_information_text;
+    private EditText name;
+    private CheckBox name_check;
+    private EditText group;
+    private CheckBox group_check;
+    int check1 = 0;
+    int check2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var04_main);
+
+        navigate_to_second_activity = (Button) findViewById(R.id.navigate_to_second_activity);
+
+        display_information_text = (TextView) findViewById(R.id.display_information_text);
+
+        display_information = (Button) findViewById(R.id.display_information);
+        //display_information.setOnClickListener(buttonClickListener);
+
+        name = (EditText) findViewById(R.id.name);
+
+        name_check = (CheckBox) findViewById(R.id.name_check);
+
+        group = (EditText) findViewById(R.id.group);
+
+        group_check = (CheckBox) findViewById(R.id.group_check);
+
+        if (savedInstanceState != null) {
+            {
+                if (savedInstanceState.containsKey(Constants.USERNAME_EDIT_TEXT)) {
+                    EditText usernameEditText = (EditText) findViewById(R.id.name);
+                    usernameEditText.setText(savedInstanceState.getString(Constants.USERNAME_EDIT_TEXT));
+                }
+                if (savedInstanceState.containsKey(Constants.GROUP_EDIT_TEXT)) {
+                    EditText passwordEditText = (EditText) findViewById(R.id.group);
+                    passwordEditText.setText(savedInstanceState.getString(Constants.GROUP_EDIT_TEXT));
+                }
+                if (savedInstanceState.containsKey(Constants.REMEMBER_NAME)) {
+                    CheckBox rememberMeCheckbox = (CheckBox) findViewById(R.id.name_check);
+                    rememberMeCheckbox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_NAME));
+                }
+                if (savedInstanceState.containsKey(Constants.REMEMBER_GROUP)) {
+                    CheckBox rememberMeCheckbox = (CheckBox) findViewById(R.id.group_check);
+                    rememberMeCheckbox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_GROUP));
+                }
+            }
+        }
     }
+
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String nume = name.getText().toString();
+            String grupa = group.getText().toString();
+           // String viewText = display_information_text.getText().toString();
+
+
+            if(view.getId() == R.id.display_information && group_check.isChecked() && name_check.isChecked()) {
+                
+            }
+
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        if (group_check.isChecked() && name_check.isChecked()) {
+            savedInstanceState.putString(Constants.USERNAME_EDIT_TEXT, name.getText().toString());
+            savedInstanceState.putString(Constants.GROUP_EDIT_TEXT, group.getText().toString());
+            savedInstanceState.putBoolean(Constants.REMEMBER_NAME, name_check.isChecked());
+            savedInstanceState.putBoolean(Constants.REMEMBER_GROUP, group_check.isChecked());
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey(Constants.USERNAME_EDIT_TEXT)) {
+            EditText usernameEditText = (EditText)findViewById(R.id.name);
+            usernameEditText.setText(savedInstanceState.getString(Constants.USERNAME_EDIT_TEXT));
+        }
+        if (savedInstanceState.containsKey(Constants.GROUP_EDIT_TEXT)) {
+            EditText passwordEditText = (EditText)findViewById(R.id.group);
+            passwordEditText.setText(savedInstanceState.getString(Constants.GROUP_EDIT_TEXT));
+        }
+        if (savedInstanceState.containsKey(Constants.REMEMBER_NAME)) {
+            CheckBox rememberMeCheckBox = (CheckBox) findViewById(R.id.name_check);
+            rememberMeCheckBox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_NAME));
+        }
+        if (savedInstanceState.containsKey(Constants.REMEMBER_GROUP)) {
+            CheckBox rememberMeCheckBox = (CheckBox) findViewById(R.id.group_check);
+            rememberMeCheckBox.setChecked(savedInstanceState.getBoolean(Constants.REMEMBER_GROUP));
+        }
+    }
+
+
 }
